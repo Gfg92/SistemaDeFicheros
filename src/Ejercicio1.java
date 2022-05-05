9275c3819c3471c463caa97b35aba0358732929e
Guillermo Fernandez <alufer3712@ieselcaminas.org>
2022-05-05 12:22:15 +0200

    Ficheros

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -1,16 +1,19 @@
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Ejercicio1 {
     private File file = new File("c:'\'");
 
     public void mostrarRaiz() {
         file = file.listRoots()[0];
-
-        int indice = 0;
-        for (String c : file.list()) {
-            System.out.println(indice + ". " + c);
-            indice++;
+        int i = 0;
+        for (String f : file.list()) {
+            System.out.println(i + ". " + f);
+            i++;
         }
+
     }
+
 
 }
